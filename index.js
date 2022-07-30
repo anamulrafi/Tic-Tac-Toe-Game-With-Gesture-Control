2701fdd19f2d6b08de66689d5b983097dc06df2e
Rafi111th <anamulhoquerafi118399@gmail.com>
2022-07-30 17:39:35 +0600

    game initially  complete & gesture control part is remaining

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Welcome to Tic Tac Toe")
 let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
-let turn = "X";
+let turn = "X"
 let isgameover = false;
 
 // Function to change the turn (x-0-x)
@@ -14,7 +14,7 @@ const changeTurn = ()=>{
 
 // Function to check for a win
 const checkWin = ()=>{
-    let itemtext = document.getElementsByClassName('itemtext');
+    let itemText = document.getElementsByClassName('itemText');
     let wins = [
         [0, 1, 2, 5, 5, 0],
         [3, 4, 5, 5, 15, 0],
@@ -26,10 +26,10 @@ const checkWin = ()=>{
         [2, 4, 6, 5, 15, 135],
     ]
     wins.forEach(e =>{
-        if((itemtext[e[0]].innerText === itemtext[e[1]].innerText) && (itemtext[e[2]].innerText === itemtext[e[1]].innerText) && (itemtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = itemtext[e[0]].innerText + " Won"
+        if((itemText[e[0]].innerText === itemText[e[1]].innerText) && (itemText[e[2]].innerText === itemText[e[1]].innerText) && (itemText[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = itemText[e[0]].innerText + " Won"
             isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            document.querySelector('.images').getElementsByTagName('img')[0].style.width = "200px";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
             document.querySelector(".line").style.width = "20vw";
         }
@@ -40,10 +40,10 @@ const checkWin = ()=>{
 // music.play()
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
-    let itemtext = element.querySelector('.itemtext');
+    let itemText = element.querySelector('.itemText');
     element.addEventListener('click', ()=>{
-        if(itemtext.innerText === ''){
-            itemtext.innerText = turn;
+        if(itemText.innerText === ''){
+            itemText.innerText = turn;
             turn = changeTurn();
             audioTurn.play();
             checkWin();
@@ -52,4 +52,16 @@ Array.from(boxes).forEach(element =>{
             } 
         }
     })
+})
+// Add onclick listener to reset button
+reset.addEventListener('click', ()=>{
+    let itemTexts = document.querySelectorAll('.itemText');
+    Array.from(itemTexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"; 
+    isgameover = false
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.querySelector('.images').getElementsByTagName('img')[0].style.width = "0px"
 })
